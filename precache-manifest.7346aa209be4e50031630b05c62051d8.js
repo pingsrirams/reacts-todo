c0741ee277222564671f6896b8cec071e7037461
Sriram <pingsrirams@gmail.com>
2019-10-24 21:37:01 -0700

    Updates

---
+++ precache-manifest.7346aa209be4e50031630b05c62051d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74051eb53bef87d45697e5582407419",
+    "url": "/reacts-todo/index.html"
+  },
+  {
+    "revision": "2306b3acfc7cba8ae322",
+    "url": "/reacts-todo/static/css/main.d68f79e1.chunk.css"
+  },
+  {
+    "revision": "c035c5e90f9a38c04412",
+    "url": "/reacts-todo/static/js/2.c6e4f528.chunk.js"
+  },
+  {
+    "revision": "2306b3acfc7cba8ae322",
+    "url": "/reacts-todo/static/js/main.10d896f3.chunk.js"
+  },
+  {
+    "revision": "ba3e5f98dbd51357de4f",
+    "url": "/reacts-todo/static/js/runtime-main.aeed949d.js"
+  }
+]);
